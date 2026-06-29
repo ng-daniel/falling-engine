ec8ae10e9f1234c34f7930444436f1e1e11dba21
Daniel Ng <ng17daniel@gmail.com>
2026-06-28 22:24:40 -0400

    layout for asset loading module

--- engine/include/engine/core/application.h
+++ engine/include/engine/core/application.h
@@ -1,8 +1,8 @@
+/// @brief Main application class that manages the game loop and overall application state.
 class Application {
 public:
     Application();
     ~Application();
 
-    void Init();
     void Run();
 };
--- engine/src/core/application.cpp
+++ engine/src/core/application.cpp
@@ -1,24 +1,13 @@
 #include <iostream>
 
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
 #include "engine/core/application.h"
 
 Application::Application() {
+    std::cout << "Application Initialized!" << std::endl;
     return;
 }
 
 Application::~Application() {
-    return;
-}
-
-void Application::Init() {
-    std::cout << "Application Initialized!" << std::endl;
     return;
 }
 
