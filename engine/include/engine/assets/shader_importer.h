8ab222707600f83559b6622fc8cfe4fe8344b39b
Daniel Ng <ng17daniel@gmail.com>
2026-07-05 16:14:10 -0400

    shader importer implemented

---
+++ engine/include/engine/assets/shader_importer.h
@@ -0,0 +1,33 @@
+#ifndef ENGINE_ASSETS_SHADER_IMPORTER_H
+#define ENGINE_ASSETS_SHADER_IMPORTER_H
+
+#include <vector>
+#include "asset_structures.h"
+
+enum ShaderType {
+    Vertex,
+    Fragment
+};
+
+struct ShaderAsset : public Asset {
+    ShaderType shaderType;
+    std::string shaderSource;
+};
+
+/**
+ * @brief Handles loading of shader assets.
+ */
+class ShaderImporter : public AssetImporter {
+public:
+    ShaderImporter() = default;
+    ~ShaderImporter() override = default;
+
+    std::unique_ptr<Asset> LoadAsset(const std::filesystem::path& path) override;
+    std::string_view GetName() override { return importerName; }
+    std::string_view GetType() override { return importerType; }
+private:
+    std::string_view importerName = "ShaderImporter"; // should be same as class name
+    std::string_view importerType = "Shader"; // should be same as enum in asset_structures
+};
+
+#endif // ENGINE_ASSETS_SHADER_IMPORTER_H
