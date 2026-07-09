01107659a126e88c2bb7d44223cc3eaeb735762b
Daniel Ng <ng17daniel@gmail.com>
2026-07-08 22:42:53 -0400

    gltf loading data structures

--- engine/include/engine/assets/asset_data.h
+++ engine/include/engine/assets/asset_data.h
@@ -3,6 +3,8 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
+#include <glm/glm.hpp>
 
 #include "engine/utils/uuid.h"
 
@@ -23,13 +25,6 @@ struct Asset {
     AssetType type;
 };
 
-struct TextureAsset : public Asset {
-    int width;
-    int height;
-    int numChannels;
-    std::vector<unsigned char> data;
-};
-
 struct ShaderAsset : public Asset {    
     enum ShaderType {
         Vertex,
@@ -37,6 +32,61 @@ struct ShaderAsset : public Asset {
     };
     ShaderType shaderType;
     std::string shaderSource;
+};
+
+struct TextureAsset : public Asset {
+    int width;
+    int height;
+    int numChannels;
+    std::vector<unsigned char> data;
+};
+
+struct MaterialAsset : public Asset
+{
+    enum class MaterialType
+    {
+        PBRMetallicRoughness,
+        Unlit,
+        Custom
+    };
+    MaterialType materialType;
+    glm::vec4 baseColorFactor = glm::vec4(1.0f);
+
+    float metallicFactor = 1.0f;
+    float roughnessFactor = 1.0f;
+    float occlusionFactor = 1.0f;
+    float normalFactor = 1.0f;
+
+    glm::vec3 emissiveFactor = glm::vec3(0.0f);
+
+    UUID baseColorTexture;          // diffuse texture
+    UUID metallicRoughnessTexture;  // metallic (G), roughness (B)
+    UUID normalTexture;             // adjusts lighting with normals
+    UUID occlusionTexture;          // make things darker
+    UUID emissiveTexture;           // make things look like glow
+};
+
+struct Vertex
+{
+    glm::vec3 position;
+    glm::vec3 normal;
+    glm::vec4 tangent;   // derive bitangent & calculate normal mapping
+    glm::vec2 texCoord;
+};
+
+struct MeshAsset : public Asset
+{
+    std::vector<Vertex> vertices;
+    std::vector<uint32_t> indices;
+};
+
+struct ModelAsset : public Asset
+{
+    std::vector<UUID> meshes;
+    std::vector<UUID> materials;
+    std::vector<UUID> textures;
+
+    UUID defaultScene;
 };
 
 #endif // ENGINE_ASSETS_ASSET_DATA_H
