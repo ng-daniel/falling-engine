c3fc53a846c6e67944613ac2f7737e101b7f87d2
Daniel Ng <ng17daniel@gmail.com>
2026-07-19 22:24:06 -0400

    resolved export name collision

--- game/include/generated/asset_ids.h
+++ game/include/generated/asset_ids.h
@@ -6,10 +6,12 @@
 
 namespace GameAssets
 {
+    constexpr AssetHandle _{ 0 };
     constexpr AssetHandle BASICFRAG_SHADER{ 17873746225813772056 };
-    constexpr AssetHandle LOOKALIKE_CROP_IMAGE{ 14755153773422602874 };
     constexpr AssetHandle BASICVERT_SHADER{ 9187245430687615186 };
     constexpr AssetHandle RYUJINCHROMATIC_IMAGE{ 4181829728652363712 };
-    constexpr AssetHandle RYUJINTEX_IMAGE{ 17286383764878019981 };
+    constexpr AssetHandle LOOKALIKE_CROP_IMAGE{ 14755153773422602874 };
+    constexpr AssetHandle RYUJINTEXPNG_IMAGE{ 17286383764878019981 };
+    constexpr AssetHandle RYUJINTEX_IMAGE{ 3811269488652409328 };
 }
 #endif // ASSET_IDS_H
